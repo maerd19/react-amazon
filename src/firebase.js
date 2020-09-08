b4669fd074d6076b0695b5fa2029cd968b66d643
maerd19 <revolutions_333@hotmail.com>
2020-09-08 18:02:09 -0500

    Header Component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyByZosXGt5276yYd_5-yz31HGztC31UH5Y",
+  authDomain: "challenge-4d263.firebaseapp.com",
+  databaseURL: "https://challenge-4d263.firebaseio.com",
+  projectId: "challenge-4d263",
+  storageBucket: "challenge-4d263.appspot.com",
+  messagingSenderId: "285298135362",
+  appId: "1:285298135362:web:8871f667861aa3692738db",
+  measurementId: "G-3MW1E9JDS4",
+};
