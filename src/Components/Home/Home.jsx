5a951091122694dae85b2b09307809cbd39ae9fc
maerd19 <revolutions_333@hotmail.com>
2020-09-09 07:22:16 -0500

    Use of Context API

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -14,7 +14,8 @@ const Home = () => {
 
         <div className="home__row">
           <Product
-            title="The lean startup"
+            id="12321341"
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
             price={29.99}
             image={
               "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "../../StateProvider";
 
 const Product = ({ id, title, image, price, rating }) => {
+  const [{ basket }, dispatch] = useStateValue();
+
+  console.log("this is the basket >>>", basket);
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -21,7 +40,7 @@ const Product = ({ id, title, image, price, rating }) => {
 
       <img src={image} alt={title} />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 };
