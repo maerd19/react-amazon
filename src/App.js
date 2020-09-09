a9a61e5f324624af8586d82756b65f13b751e7fc
maerd19 <revolutions_333@hotmail.com>
2020-09-09 06:54:49 -0500

    Checkout & Subtotal Components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
+import Checkout from "./Components/Checkout/Checkout";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
